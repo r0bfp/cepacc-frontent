3770825614132efd530f56cd6afe6a4773f22619
r0bfp <robfpinheiro@gmail.com>
2021-11-09 00:48:29 -0300

    feat: add page institucional (without implementation)

---
+++ src/pages/institucional/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import { MainContainer } from "./style";
+import { Typography } from "antd";
+
+const { Title, Paragraph } = Typography;
+
+export default function Institucional(){
+    return (
+        <MainContainer>
+            <Typography>
+                <Title>Quem somos</Title>
+                <Paragraph>loreaiushdiaushdiaushdiauhsdiuashdiuahsdiuahsiduhasiud</Paragraph>
+            </Typography>
+        </MainContainer>
+    )
+}
---
+++ src/pages/institucional/style.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const MainContainer = styled.div`
+    width: 100%;
+    height: 100%;
+    padding: 15px;
+
+    border: 1px solid #f0f0f0;
+`;
+
+export const ParagraphContainer = styled.div`
+
+`;
