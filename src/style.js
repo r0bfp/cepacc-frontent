0b437a8ca262a52458d423a654f007a6e965e6dc
r0bfp <robfpinheiro@gmail.com>
2021-11-09 00:46:19 -0300

    feat: add app main style

---
+++ src/style.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const HeaderContainer = styled.div`
+    width: 100%;
+    height: 46px;
+`;
+
+export const BodyContainer = styled.div`
+    width: 100%;
+    height: 100%;
+    padding: 40px;
+    padding-top: 80px;
+`;
