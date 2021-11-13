a206c4f0e53c668a5369ffe2f69e45f179d89501
r0bfp <robfpinheiro@gmail.com>
2021-11-13 13:28:17 -0300

    feat: center objects

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,37 +3,41 @@ import { Link } from "react-router-dom";
 import { Menu, Button } from "antd";
 import { HomeOutlined, BankOutlined, BookOutlined, UserOutlined } from "@ant-design/icons";
 
-import { LogoContainer, MenuContainer, MainContainer, ButtonContainer } from "./style";
+import { LogoContainer, MenuContainer, MainContainer, ButtonContainer, MainContent } from "./style";
+import logo from "../../assets/logo.png";
+
 
 export default function Header() {
     return (
         <MainContainer>
-            <LogoContainer>
-                Logo
-            </LogoContainer>
-            <MenuContainer>
-                <Menu mode="horizontal" style={{ display: "flex", justifyContent: "right" }}>
-                    <Menu.Item key="inicio" icon={<HomeOutlined />}>
-                        <Link to='/'/>
-                        Inicio
-                    </Menu.Item>
-                    <Menu.Item key="institucional" icon={<BankOutlined />}>
-                        <Link to='/institucional'/>
-                        Institucional
-                    </Menu.Item>
-                    <Menu.Item key="cursos" icon={<BookOutlined />}>
-                        <Link to='/cursos'/>
-                        Cursos
-                    </Menu.Item>
-                    <Menu.Item key="area-do-aluno" icon={<UserOutlined />}>
-                        <Link to='/login'/>
-                        Área do Aluno
-                    </Menu.Item>
-                </Menu>
-            </MenuContainer>
-            <ButtonContainer>
-                <Button type="primary" shape="round">Matricule-se!</Button>
-            </ButtonContainer>
+            <MainContent>
+                <LogoContainer>
+                    <img src={logo} alt='logo'/>
+                </LogoContainer>
+                <MenuContainer>
+                    <Menu defaultSelectedKeys='inicio' mode="horizontal" style={{ display: "flex", justifyContent: "right" }}>
+                        <Menu.Item key="inicio" icon={<HomeOutlined />}>
+                            <Link to='/'/>
+                            Inicio
+                        </Menu.Item>
+                        <Menu.Item key="institucional" icon={<BankOutlined />}>
+                            <Link to='/institucional'/>
+                            Institucional
+                        </Menu.Item>
+                        <Menu.Item key="cursos" icon={<BookOutlined />}>
+                            <Link to='/cursos'/>
+                            Cursos
+                        </Menu.Item>
+                        <Menu.Item key="login" icon={<UserOutlined />}>
+                            <Link to='/login'/>
+                            Área do Aluno
+                        </Menu.Item>
+                    </Menu>
+                </MenuContainer>
+                <ButtonContainer>
+                    <Button type="primary" shape="round">Matricule-se!</Button>
+                </ButtonContainer>
+            </MainContent>
         </MainContainer>
     )
 }
--- src/components/Header/style.js
+++ src/components/Header/style.js
@@ -3,29 +3,39 @@ import styled from "styled-components";
 export const MainContainer = styled.div`
     width: 100%;
     height: 46px;
+    background-color: #FFFF;
+    
+    position: fixed;
+    top: 0;
+    `;
+
+export const MainContent = styled.div`
     display: flex;
     flex-direction: row;
     justify-content: space-between;
-    padding: 0 40px;
-    background-color: #FFFF;
 
-    position: fixed;
-    top: 0;
+    max-width: 1400px;
+    padding: 0 40px;
+    margin: 0 auto;
 `;
 
 export const LogoContainer = styled.div`
-    width: 100%;
+    width: 65px;
+    max-height: 45px;
     display: flex;
     align-items: center;
     justify-content: center;
     border-bottom: 1px solid #f0f0f0;
+
+    img{
+        width: 65px;
+        height: 100%;
+    }
 `;
 
 export const ButtonContainer = styled.div`
-    width: 100%;
     display: flex;
     align-items: center;
-    justify-content: center;
     border-bottom: 1px solid #f0f0f0;
 `;
 
