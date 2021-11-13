12f032390d0e256b0db59e6d154999cadaf1d0b5
r0bfp <robfpinheiro@gmail.com>
2021-11-13 13:27:28 -0300

    feat: center content

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -1,37 +1,39 @@
 import React from "react";
 import { Carousel as AntdCarousel, Typography} from "antd";
 
-import { MainContainer, CarouselImageContainer, MessageContainer } from "./style";
+import { MainContainer, CarouselImageContainer, MessageContainer, CarouselContainer } from "./style";
 
 export default function Carousel() {
     return (
         <MainContainer>
-            <AntdCarousel autoplay>
-                <CarouselImageContainer>
-                    <MessageContainer>
-                        <Typography.Title>Titulo 1</Typography.Title>
-                        <Typography.Paragraph>Mensagem 1</Typography.Paragraph>
-                    </MessageContainer>
-                </CarouselImageContainer>
-                <CarouselImageContainer>
-                    <MessageContainer>
-                        <Typography.Title>Titulo 2</Typography.Title>
-                        <Typography.Paragraph>Mensagem 2</Typography.Paragraph>
-                    </MessageContainer>
-                </CarouselImageContainer>
-                <CarouselImageContainer>
-                    <MessageContainer>
-                        <Typography.Title>Titulo 3</Typography.Title>
-                        <Typography.Paragraph>Mensagem 3</Typography.Paragraph>
-                    </MessageContainer>
-                </CarouselImageContainer>
-                <CarouselImageContainer>
-                    <MessageContainer>
-                        <Typography.Title>Titulo 4</Typography.Title>
-                        <Typography.Paragraph>Mensagem 4</Typography.Paragraph>
-                    </MessageContainer>
-                </CarouselImageContainer>
-            </AntdCarousel>
+            <CarouselContainer>
+                <AntdCarousel autoplay>
+                    <CarouselImageContainer>
+                        <MessageContainer>
+                            <Typography.Title>Titulo 1</Typography.Title>
+                            <Typography.Paragraph>Mensagem 1</Typography.Paragraph>
+                        </MessageContainer>
+                    </CarouselImageContainer>
+                    <CarouselImageContainer>
+                        <MessageContainer>
+                            <Typography.Title>Titulo 2</Typography.Title>
+                            <Typography.Paragraph>Mensagem 2</Typography.Paragraph>
+                        </MessageContainer>
+                    </CarouselImageContainer>
+                    <CarouselImageContainer>
+                        <MessageContainer>
+                            <Typography.Title>Titulo 3</Typography.Title>
+                            <Typography.Paragraph>Mensagem 3</Typography.Paragraph>
+                        </MessageContainer>
+                    </CarouselImageContainer>
+                    <CarouselImageContainer>
+                        <MessageContainer>
+                            <Typography.Title>Titulo 4</Typography.Title>
+                            <Typography.Paragraph>Mensagem 4</Typography.Paragraph>
+                        </MessageContainer>
+                    </CarouselImageContainer>
+                </AntdCarousel>
+            </CarouselContainer>
         </MainContainer>
     )
 }
--- src/components/Carousel/style.js
+++ src/components/Carousel/style.js
@@ -4,6 +4,11 @@ export const MainContainer = styled.div`
     padding: 0 40px;
 `;
 
+export const CarouselContainer = styled.div`
+    max-width: 1200px;
+    margin: 0 auto;
+`;
+
 export const CarouselImageContainer = styled.div`
     height: 450px;
     background-color: grey;
