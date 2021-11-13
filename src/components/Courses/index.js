c9f1e28cfa19e797caef7a65e20f5ec92672d561
r0bfp <robfpinheiro@gmail.com>
2021-11-13 13:28:02 -0300

    feat: add page courses

---
+++ src/components/Courses/index.js
@@ -0,0 +1,76 @@
+import React from "react";
+import { MdWatchLater } from "react-icons/md"
+import { GiGraduateCap } from "react-icons/gi"
+
+import { 
+    Course,
+    CourseInfoContainer,
+    CourseInfosContainer,
+    CourseModalities,
+    CoursesContainer,
+    CoursesListContainer,
+    CourseTitle,
+    ListTitle,
+    MainContainer
+} from "./style";
+
+export default function Courses(props) {
+    const apiCourseMock = [
+        {
+            nome: 'A Arte de Contar Histórias',
+            area: 'Educação',
+            duracao: '450h',
+            tipo: 'Pós-Graduação',
+            tbl_modalidade: [
+                'ead',
+                'presencial'
+            ]
+        },
+        {
+            nome: 'A Arte de Contar Histórias',
+            area: 'Educação',
+            duracao: '450h',
+            tipo: 'Pós-Graduação',
+            tbl_modalidade: [
+                'ead',
+                'presencial'
+            ]
+        }
+    ]
+
+    return (
+        <MainContainer>
+            <CoursesContainer>
+                <ListTitle>{props.courseType}</ListTitle>
+                <CoursesListContainer>
+                    {
+                        apiCourseMock.map(course => {
+                            return (
+                                <Course courseType={props.courseType}>
+                                    <CourseTitle>{course.nome}</CourseTitle>
+                                    {
+                                        course.tbl_modalidade.map((modality, index) => {
+                                            return (
+                                                <CourseModalities> {index !== 0 && "/"} {modality}</CourseModalities>
+                                            )
+                                        })
+                                    }
+                                    <CourseInfosContainer>
+                                        <CourseInfoContainer>
+                                            <MdWatchLater size={20} color='rgb(225 180 80)'/>
+                                            <label>{course.duracao}</label>
+                                        </CourseInfoContainer>
+                                        <CourseInfoContainer>
+                                            <GiGraduateCap size={25} color='rgb(225 180 80)'/>
+                                            <label>{course.area}</label>
+                                        </CourseInfoContainer>
+                                    </CourseInfosContainer>
+                                </Course>
+                            )
+                        })
+                    }
+                </CoursesListContainer>
+            </CoursesContainer>
+        </MainContainer>
+    )
+}
