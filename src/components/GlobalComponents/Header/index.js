c3a9bcb8f6b9f68139c5168d0e72927808f2a7b4
Robert <robfpinheiro@gmail.com>
2021-11-25 23:28:44 -0300

    refactor: change antd header to custom

--- src/components/GlobalComponents/Header/index.js
+++ src/components/GlobalComponents/Header/index.js
@@ -1,33 +1,59 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { Menu, Button } from "antd";
+import { Button } from "antd";
 import { HomeOutlined, BankOutlined, BookOutlined, UserOutlined } from "@ant-design/icons";
 
-import { LogoContainer, MenuContainer, MainContainer, ButtonContainer, MainContent } from "./style";
+import { 
+    LogoContainer, 
+    Menu, 
+    MainContainer, 
+    MainContent,
+    MenuItem,
+    Text,
+    IconContainer,
+    TextContainer,
+    EmphasisButtonContainer
+} from "./style";
 import logo from "../../../assets/logo.png";
 
 
-export default function Header(props) {
-    // TODO setar pagina atual ao recaregar
-    // const [ currentPage, setCurrentPage ] = useState('/');
+export default function Header() {
+    const [ tabs, setTabs ] = useState([
+        {
+            title: 'Início',
+            icon: (<HomeOutlined/>),
+            link: '/',
+            selected: false
+        },
+        {
+            title: 'Institucional',
+            icon: (<BankOutlined/>),
+            link: '/institucional',
+            selected: true
+        },
+        {
+            title: 'Cursos',
+            icon: (<BookOutlined/>),
+            link: '/cursos',
+            selected: false
+        },
+        {
+            title: 'Área do Aluno',
+            icon: (<UserOutlined/>),
+            link: '/area-do-aluno/login',
+            selected: false
+        },
+    ])
 
-    // function setDefaultSelectedKeysByRoute() {
-    //     const routesDictonary = {
-    //         '/': 'inicio',
-    //         '/institucional': 'institucional',
-    //         '/cursos': 'cursos',
-    //         '/area-do-aluno/login': 'login'
-    //     };
-    //     const currentLocation = window.location.href;
-    //     const fullHostname = currentLocation.split('/').slice(0, 3).join('/');
-    //     const currentRoute = currentLocation.replace(fullHostname, '');
+    const currentLocation = window.location.href;
+    const fullHostname = currentLocation.split('/').slice(0, 3).join('/');
+    const currentRoute = currentLocation.replace(fullHostname, '');
 
-    //     setCurrentPage(routesDictonary[currentRoute]);
-    // }
-
-    // useEffect(() => {
-    //     setDefaultSelectedKeysByRoute();
-    // }, [])
+    function handleTabSelected(tabTitle) {
+        setTabs(prev => prev.map(e => {
+            return tabTitle === e.title ? {...e, selected: true} : {...e, selected: false}
+        }))
+    }
 
     return (
         <MainContainer>
@@ -35,29 +61,29 @@ export default function Header(props) {
                 <LogoContainer>
                     <img src={logo} alt='logo'/>
                 </LogoContainer>
-                <MenuContainer>
-                    <Menu defaultSelectedKeys='inicio' mode="horizontal" style={{ display: "flex", justifyContent: "right" }}>
-                        <Menu.Item key="inicio" icon={<HomeOutlined />}>
-                            <Link to='/'/>
-                            Inicio
-                        </Menu.Item>
-                        <Menu.Item key="institucional" icon={<BankOutlined />}>
-                            <Link to='/institucional'/>
-                            Institucional
-                        </Menu.Item>
-                        <Menu.Item key="cursos" icon={<BookOutlined />}>
-                            <Link to='/cursos'/>
-                            Cursos
-                        </Menu.Item>
-                        <Menu.Item key="login" icon={<UserOutlined />}>
-                            <Link to='/area-do-aluno/login'/>
-                            Área do Aluno
-                        </Menu.Item>
-                    </Menu>
-                </MenuContainer>
-                <ButtonContainer>
-                    <Button type="primary" shape="round">Matricule-se!</Button>
-                </ButtonContainer>
+                <Menu>
+                    {
+                        tabs.map((e, i) => {
+                            return (
+                                <Link to={e.link}>
+                                    <MenuItem key={i} selected={e.selected} onClick={() => handleTabSelected(e.title)}>
+                                        <IconContainer>
+                                            {e.icon}
+                                        </IconContainer>
+                                        <TextContainer>
+                                            <Text>{e.title}</Text>
+                                        </TextContainer>
+                                    </MenuItem>
+                                </Link>
+                            )
+                        })
+                    }
+                </Menu>
+                <EmphasisButtonContainer>
+                    <Link to='/matricule-se'>
+                        <Button type="primary" shape="round" size='large'>Matricule-se!</Button>
+                    </Link>
+                </EmphasisButtonContainer>
             </MainContent>
         </MainContainer>
     )
--- src/components/GlobalComponents/Header/style.js
+++ src/components/GlobalComponents/Header/style.js
@@ -1,9 +1,11 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+import { AppColors } from "../../../globalStyle";
 
 export const MainContainer = styled.div`
     width: 100%;
-    height: 46px;
+    height: 70px;
     background-color: #FFFF;
+    border-bottom: 1px solid #f0f0f0;
 
     position: fixed;
     top: 0;
@@ -11,37 +13,96 @@ export const MainContainer = styled.div`
 `;
 
 export const MainContent = styled.div`
+    height: 100%;
+    max-width: 1200px;
+
     display: flex;
     flex-direction: row;
-    justify-content: space-between;
-
-    max-width: 1400px;
-    padding: 0 40px;
     margin: 0 auto;
 `;
 
+
 export const LogoContainer = styled.div`
-    width: 65px;
-    max-height: 45px;
+    height: 100%;
     display: flex;
-    align-items: center;
     justify-content: center;
-    border-bottom: 1px solid #f0f0f0;
+    padding: 5px;
 
     img{
-        width: 65px;
         height: 100%;
+        object-fit: contain;
     }
 `;
 
-export const ButtonContainer = styled.div`
+export const Menu = styled.ul`
+    list-style-type: none;
+    margin: 0;
+
+    width: 100%;
+    height: 100%;
     display: flex;
-    align-items: center;
-    border-bottom: 1px solid #f0f0f0;
+    justify-content: end;
 `;
 
-export const MenuContainer = styled.div`
-    min-width: 80%;
+export const MenuItemAfter = css`
+    height: 2px;
+    background-color: #096FB5;
+`;
+
+export const MenuItem = styled.li`
+    height: 100%;
+    display: flex;
+    font-size: 16px;
+    margin-right: 10px;
+    cursor: pointer;
+    position: relative;
+    transition: 200ms cubic-bezier(.645,.045,.355,1);
+    color: ${props => props.selected ? '#096FB5' : '#000000d9'};
+
+    &::after{
+        content: '';
+        width: 100%;
+        display: block;
+        position: absolute;
+        bottom: 0;
+        border-radius: 20%;
+        transition: .3s cubic-bezier(.645,.045,.355,1);
+
+        ${props => props.selected && MenuItemAfter};
+    }
+    
+    &:hover{
+        color: #096FB5;
+    }
+
+    &:hover::after{
+        ${MenuItemAfter}
+    }
+`;
+
+export const IconContainer = styled.div`
+    height: 100%;
+    display: flex;
+    align-items: center;
+    padding: 0 5px 0 10px;
+`;
+
+export const TextContainer = styled.div`
+    height: 100%;
+    display: flex;
+    align-items: center;
+    padding: 0 10px 0 5px;
+`;
+
+export const Text = styled.span`
+
+`;
+
+export const EmphasisButtonContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-left: 15px;
 `;
 
 
