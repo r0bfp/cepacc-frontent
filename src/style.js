f26c723627c127898fd7c8b37215b418d6eb71e9
Robert <robfpinheiro@gmail.com>
2021-12-09 22:59:59 -0300

    style: change dimations

--- src/style.js
+++ src/style.js
@@ -1,13 +1,7 @@
 import styled from "styled-components";
 
-export const HeaderContainer = styled.div`
-    width: 100%;
-    height: 46px;
-`;
-
 export const BodyContainer = styled.div`
     width: 100%;
     height: 100%;
-    /* padding-top: 80px; */
-    background-color: #F5F6FC;
+    padding-top: 100px;
 `;
