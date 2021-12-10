5717d10dd66b6a25e2ecbfa59c72cee63e900999
Robert <robfpinheiro@gmail.com>
2021-12-09 23:02:35 -0300

    style: change contact us style

--- src/components/GlobalComponents/ContactUs/index.js
+++ src/components/GlobalComponents/ContactUs/index.js
@@ -1,12 +1,11 @@
 import React from "react";
-import { IoLogoWhatsapp } from "react-icons/io";
+import { MainContainer, Message } from "./style";
 
-import { MainContainer } from "./style";
 
 export default function ContactUs() {
     return (
         <MainContainer>
-            <IoLogoWhatsapp size={45} color={'#0B9F14'}/>
+            <Message>FALE CONOSCO</Message>
         </MainContainer>
     )
 }
--- src/components/GlobalComponents/ContactUs/style.js
+++ src/components/GlobalComponents/ContactUs/style.js
@@ -2,8 +2,6 @@ import styled from "styled-components";
 
 export const MainContainer = styled.div`
     position: fixed;
-    width: 60px;
-    height: 60px;
     bottom: 30px;
     right: 30px;
 
@@ -11,8 +9,16 @@ export const MainContainer = styled.div`
     justify-content: center;
     align-items: center;
 
-    border-radius: 50%;
+    border-radius: 10px;
     box-shadow: 0 3px 6px -4px #0000001f, 0 6px 16px #00000014, 0 9px 28px 8px #0000000d;
-    background-color: #FFFFFF;
     cursor: pointer;
+
+    padding: 20px;
+    background-color: #01468C;
+`;
+
+export const Message = styled.span`
+    font-weight: 800;
+    font-size: 16px;
+    color: #FFF;
 `;
