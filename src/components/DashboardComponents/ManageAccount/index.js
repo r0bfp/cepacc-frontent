9eb18682d4c1d0a0df416bb427313f2c5c991bb4
r0bfp <robfpinheiro@gmail.com>
2022-03-03 23:49:12 -0300

    style:  layout changed

--- src/components/DashboardComponents/ManageAccount/index.js
+++ src/components/DashboardComponents/ManageAccount/index.js
@@ -1,12 +1,11 @@
 import React, { useState } from "react";
-import { Form, Input, Button, Divider, DatePicker, Select, InputNumber } from "antd";
+import { Form, Input, Button, DatePicker, Select, InputNumber } from "antd";
 
 import { 
-    ButtonSendContainer, 
-    PersonalDataContainer, 
-    MainContainer, 
-    Title, 
-    AddressDataContainer 
+    ContentContainer,
+    Divider,
+    AddressDataContainer,
+    MainContainer, PersonalDataContainer, Title, SendButtonContainer, 
 } from "./style";
 
 
@@ -43,102 +42,104 @@ export default function ManageAccount() {
 
     return (
         <MainContainer>
-            <Title>Dados Pessoais</Title>
-            <Form layout='vertical'>
-                <PersonalDataContainer>
-                    <Form.Item
-                        label="Nome Completo"
-                        name="name"
-                        style={{gridArea: 'name'}}
-                    >
-                        <Input placeholder='Ex: José da Silva'/>
-                    </Form.Item>
-                    <Form.Item
-                        label="Data de Nascimento"
-                        name="birth"
-                        style={{gridArea: 'birth'}}
-                    >
-                        <DatePicker format={'DD-MM-YYYY'} />
-                    </Form.Item>
-                    <Form.Item
-                        label="CPF"
-                        name="cpf"
-                        style={{gridArea: 'cpf'}}
-                    >
-                        <Input placeholder='000.000.000-00'/>
-                    </Form.Item>
-                    <Form.Item
-                        label="RG"
-                        name="rg"
-                        style={{gridArea: 'rg'}}
-                    >
-                        <Input placeholder='00.000.000-0'/>
-                    </Form.Item>
-                    <Form.Item
-                        label="Celular"
-                        name="phone"
-                        style={{gridArea: 'phone'}}
-                    >
-                        <Input placeholder='(00) 00000-0000'/>
-                    </Form.Item>
-                </PersonalDataContainer>
-            </Form>
-            <Title>Endereço</Title>
-            <Form layout='vertical'>
-                <AddressDataContainer>
-                    <Form.Item
-                        label="CEP"
-                        name="cep"
-                        style={{gridArea: 'cep'}}
-                    >
-                        <Input/>
-                    </Form.Item>
-                    <Form.Item
-                        label="Cidade"
-                        name="city"
-                        style={{gridArea: 'city'}}
-                    >
-                        <Input/>
-                    </Form.Item>
-                    <Form.Item
-                        label="Estado"
-                        name="state"
-                        style={{gridArea: 'state'}}
-                    >
-                        <Select showSearch>
-                            {
-                                states.map((e, i) => {
-                                    return <Select.Option key={i} value={e}>{e}</Select.Option>
-                                })
-                            }
-                        </Select>
-                    </Form.Item>
-                    <Form.Item
-                        label="Rua/Avenida"
-                        name="street"
-                        style={{gridArea: 'street'}}
-                    >
-                        <Input/>
-                    </Form.Item>
-                    <Form.Item
-                        label="Bairro"
-                        name="district"
-                        style={{gridArea: 'district'}}
-                    >
-                        <Input/>
-                    </Form.Item>
-                    <Form.Item
-                        label="Número"
-                        name="number"
-                        style={{gridArea: 'number'}}
-                    >
-                        <InputNumber max={9999999}/>
-                    </Form.Item>
-                </AddressDataContainer>
-            </Form>
-            <ButtonSendContainer>
-                <Button type='primary' size="large">Salvar</Button>
-            </ButtonSendContainer>
+            <Title>Meus Dados</Title>
+            <ContentContainer>
+                <Form layout='vertical'>
+                    <PersonalDataContainer>
+                        <Form.Item
+                            label="NOME COMPLETO"
+                            name="name"
+                            style={{gridArea: 'name'}}
+                        >
+                            <Input placeholder='Ex: José da Silva'/>
+                        </Form.Item>
+                        <Form.Item
+                            label="DATA DE NASCIMENTO"
+                            name="birth"
+                            style={{gridArea: 'birth'}}
+                        >
+                            <DatePicker format={'DD-MM-YYYY'} />
+                        </Form.Item>
+                        <Form.Item
+                            label="CPF"
+                            name="cpf"
+                            style={{gridArea: 'cpf'}}
+                        >
+                            <Input placeholder='000.000.000-00'/>
+                        </Form.Item>
+                        <Form.Item
+                            label="RG"
+                            name="rg"
+                            style={{gridArea: 'rg'}}
+                        >
+                            <Input placeholder='00.000.000-0'/>
+                        </Form.Item>
+                        <Form.Item
+                            label="CELULAR"
+                            name="phone"
+                            style={{gridArea: 'phone'}}
+                        >
+                            <Input placeholder='(00) 00000-0000'/>
+                        </Form.Item>
+                    </PersonalDataContainer>
+                </Form>
+                <Divider/>
+                <Form layout='vertical'>
+                    <AddressDataContainer>
+                        <Form.Item
+                            label="CEP"
+                            name="cep"
+                            style={{gridArea: 'zipcode'}}
+                        >
+                            <Input/>
+                        </Form.Item>
+                        <Form.Item
+                            label="Cidade"
+                            name="city"
+                            style={{gridArea: 'city'}}
+                        >
+                            <Input/>
+                        </Form.Item>
+                        <Form.Item
+                            label="Estado"
+                            name="state"
+                            style={{gridArea: 'state'}}
+                        >
+                            <Select showSearch>
+                                {
+                                    states.map((e, i) => {
+                                        return <Select.Option key={i} value={e}>{e}</Select.Option>
+                                    })
+                                }
+                            </Select>
+                        </Form.Item>
+                        <Form.Item
+                            label="Rua/Avenida"
+                            name="street"
+                            style={{gridArea: 'street'}}
+                        >
+                            <Input/>
+                        </Form.Item>
+                        <Form.Item
+                            label="Bairro"
+                            name="district"
+                            style={{gridArea: 'district'}}
+                        >
+                            <Input/>
+                        </Form.Item>
+                        <Form.Item
+                            label="Número"
+                            name="number"
+                            style={{gridArea: 'number'}}
+                        >
+                            <InputNumber max={9999999}/>
+                        </Form.Item>
+                    </AddressDataContainer>
+                </Form>
+            </ContentContainer>
+            <SendButtonContainer>
+                <Button type='primary' size="large">Confirmar</Button>
+            </SendButtonContainer>
         </MainContainer>
     )
 }
--- src/components/DashboardComponents/ManageAccount/style.js
+++ src/components/DashboardComponents/ManageAccount/style.js
@@ -3,49 +3,75 @@ import styled from "styled-components";
 
 export const MainContainer = styled.div`
     width: 100%;
-    max-height: 100%;
-
-    label{
-        color: #002756;
-    }
-`;
-
-export const PersonalDataContainer = styled.div`
-    display: grid;
-    grid-column-gap: 10px;
-    grid-template-areas:
-        "name name birth"
-        "cpf rg phone";
-    background-color: #FFF;
-    border: 1px solid #F0EEED;
-    padding: 20px;
-    margin-bottom: 40px;
-`;
-
-export const AddressDataContainer = styled.div`
-    display: grid;
-    grid-column-gap: 10px;
-    grid-template-areas:
-        "cep . ."
-        "street district number"
-        "city state .";
-    margin-bottom: auto;
-    background-color: #FFF;
-    border: 1px solid #F0EEED;
-    padding: 20px;
-    margin-bottom: 40px;
+    height: 100%;
+    padding: 40px;
 `;
 
 export const Title = styled.h1`
+    font-size: 22px;
     font-weight: 700;
     color: #002756;
 `;
 
-export const ButtonSendContainer = styled.div`
+export const ContentContainer = styled.div`
+    background-color: #FFF;
     width: 100%;
-    height: 100%;
-    display: flex;
-    justify-content: end;
+    border-radius: 15px;
+    padding: 20px;
+
+    label {
+        color: #b3b9c4;
+        font-size: 12px;
+        font-weight: 600;
+    }
+
+    input { 
+        border-radius: 5px;
+    }
 `;
 
+export const Divider = styled.div`
+    width: 100%;
+    margin: 15px 0;
+    border: 1px solid #c9cbd752;
+`;
 
+export const SendButtonContainer = styled.div`
+    width: 100%;
+    padding: 15px 0;
+    display: flex;
+    justify-content: end;
+
+    button {
+        border-radius: 40px;
+    }
+`;
+
+// PERSONAL DATA =================================
+export const PersonalDataContainer = styled.div`
+    display: grid;
+    grid-column-gap: 20px;
+    grid-template-areas: 
+        "name name name"
+        "birth cpf rg"
+        "phone phone ."
+    ;
+
+    div { 
+        border-radius: 5px;
+    }
+`;
+
+// ENDRESS DATA =================================
+export const AddressDataContainer = styled.div`
+    display: grid;
+    grid-column-gap: 20px;
+    grid-template-areas: 
+        "zipcode state city"
+        "street number district"
+    ;
+
+    div, .ant-select-selector { 
+        border-radius: 5px !important; 
+    }
+`;
