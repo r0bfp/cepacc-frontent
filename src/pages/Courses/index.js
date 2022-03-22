ebbca010a0d63103e1fd8a106344b637224b7f3a
r0bfp <robfpinheiro@gmail.com>
2022-03-21 23:41:26 -0300

    feat: add custom table

--- src/pages/Courses/index.js
+++ src/pages/Courses/index.js
@@ -1,13 +1,83 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Tag } from "antd";
 
-import { MainContainer } from "./style";
-import CoursesComponent from "../../components/CoursesComponents/Courses";
+import CourseDetail from "../../components/CoursesComponents/CourseDetail";
+import NoCoursesFound from "../../components/CoursesComponents/NoCoursesFound";
+import Filters from "../../components/CoursesComponents/Filters";
+import Table from "../../components/CommonComponents/Table";
+
+import { api } from "../../services/api";
+import { FiltersContainer, MainContainer, TableContainer, Title } from "./style";
 
 export default function Courses(){
+    const [courseDetailShowing, setCourseDetailShowing] = useState(false);
+    const [courses, setCourses] = useState([]);
+    const [filteredCourses, setFilteredCourses] = useState([]);
+
+
+    useEffect(() => {
+        async function fetchData() {
+            const response = await api.get("courses/list");
+            const courses = rawToStandardizedCourses(response.data);
+
+            setCourses(courses);
+            setFilteredCourses(courses);
+        }
+
+        fetchData();
+    }, []);
+
+    const tableHead = ['Nome', 'Área', 'Tipo', 'Duração', 'Modalidade'];
+
+    // standardize courses object to format expected by the table component
+    function rawToStandardizedCourses(courses) {
+        const coursesBy = {};
+
+        courses.forEach(course => {
+            if(!coursesBy[course.type]){
+                coursesBy[course.type] = []
+            }
+
+            const { area, duration, modalities, name, type } = course;
+
+            coursesBy[course.type].push({
+                name,
+                area,
+                type,
+                duration: <span>{duration} Horas</span>,
+                render: 
+                    <span>
+                        {
+                            modalities.map((element, index) => (
+                                <Tag color='blue' key={index}>{element.name}</Tag>
+                            ))
+                        }
+                    </span>
+            });
+        });
+
+        return coursesBy;
+    }
+
+
     return (
         <MainContainer>
-            <CoursesComponent courseType='Pós-Graduação'/>
-            <CoursesComponent courseType='Curso de Extensão'/>
+            <FiltersContainer>
+                <Filters handleFilters={() => {}}/>
+            </FiltersContainer>
+            {
+                Object.keys(filteredCourses).map((courseType, index) => (
+                    <TableContainer key={index}>
+                        <Title>{courseType}</Title>
+                        <Table 
+                            tableHead={tableHead} 
+                            tableData={filteredCourses[courseType]}
+                            // handleRowClick={handleTableRowClick}
+                        />
+                    </TableContainer>
+                ))
+            }
+            <CourseDetail showing={courseDetailShowing} setCourseDetailShowing={setCourseDetailShowing}/>
         </MainContainer>
     )
 }
--- src/pages/Courses/style.js
+++ src/pages/Courses/style.js
@@ -1,7 +1,31 @@
 import styled from "styled-components";
 
+
 export const MainContainer = styled.div`
     width: 100%;
     height: 100%;
-    padding: 20px 40px;
+    padding-top: 150px;
+    position: relative;
+`;
+
+export const TableContainer = styled.div`
+    width: 100%;
+    height: 100%;
+    max-width: 1200px;
+    margin: 0 auto;
+    margin-bottom: 40px;
+`;
+
+export const FiltersContainer = styled.div`
+    width: 100%;
+    height: 100%;
+    max-width: 1200px;
+    margin: 0 auto;
+    margin-bottom: 40px;
+`;
+
+export const Title = styled.h1`
+    font-size: 30px;
+    color: #002756;
+    font-weight: 700;
 `;
